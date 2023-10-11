f275db3ba0a2c7960294f6d491d2904cbdb51a58
Benjamin Pipolo <benjaminpipolobp1@gmail.com>
2023-10-10 23:33:10 -0300

    preentrega 3

---
+++ data/stock.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Adidas",
+        tipo: "botines",
+        desc: "Botines adidas blancos",
+        precio: 150000,
+        imagen: '../public/images/adidas.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Nike amarillos",
+        tipo: "botines",
+        desc: "Botines Nikes amarillos",
+        precio: 175000,
+        imagen: '../public/images/nike-amarillos.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Nike blancos",
+        tipo: "botines",
+        desc: "Botines Nike blancos",
+        precio: 165000,
+        imagen: '../public/images/nike-blancos.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Nike negros",
+        tipo: "botines",
+        desc: "Botines Nike negros",
+        precio: 155000,
+        imagen: '../public/images/nike-negros.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Nike rojos",
+        tipo: "botines",
+        desc: "Botines Nike rojos",
+        precio: 170000,
+        imagen: '../public/images/nike-rojos.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Nike violetas",
+        tipo: "botines",
+        desc: "Botines Nike violeta",
+        precio: 180000,
+        imagen: '../public/images/nike-violeta.jpg',
+        cantidad:1
+    }
+];
